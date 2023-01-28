bb2d503255f8de8b75c67041b08c04cdb3c0d9e5
Filip Filchev <fkfilchev@gmail.com>
2023-01-28 18:57:21 +0200

    Add Spotify Client Logs

--- src/client/SpotifyClient.java
+++ src/client/SpotifyClient.java
@@ -1,5 +1,6 @@
 package client;
 
+import javax.naming.OperationNotSupportedException;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
@@ -7,12 +8,17 @@ import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class SpotifyClient implements Runnable {
+    private static final String CLIENT_LOG_FILE_NAME = "clientLogs.txt";
     private static final int RESPONSE_STATUS_INDEX = 0;
     private static final int ENCODING_INDEX = 1;
     private static final int SAMPLE_RATE_INDEX = 2;
@@ -34,53 +40,75 @@ public class SpotifyClient implements Runnable {
 
     @Override
     public void run() {
-        try (SocketChannel socketChannel = SocketChannel.open(); Scanner scanner = new Scanner(System.in)) {
+        try (SocketChannel socketChannel = SocketChannel.open(); Scanner scanner = new Scanner(System.in);
+             PrintWriter logsWriter = new PrintWriter(Files.newBufferedWriter(Path.of(CLIENT_LOG_FILE_NAME)))) {
             socketChannel.connect(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
 
             System.out.println("Connected to the server.");
             while (!end) {
-                System.out.print("Enter message: ");
-                System.out.println();
-                String message = scanner.nextLine(); // read a line from the console
+                System.out.println("Enter message: ");
+                String message = scanner.nextLine();
 
                 if ("disconnect".equals(message) || "terminate".equals(message)) {
                     end = true;
                 }
 
                 if ("stop".equalsIgnoreCase(message)) {
-                    stopSong();
+                    try {
+                        stopSong();
+                        logMessage("Song stopped", logsWriter);
+                    } catch (OperationNotSupportedException e) {
+                        logException(new Exception("Nothing to stop. No Song is playing", e), logsWriter);
+                    }
                     continue;
                 }
 
-                System.out.println("Sending message <" + message + "> to the server...");
+                logMessage("Sending message <" + message + "> to the server", logsWriter);
                 writeToServer(message, socketChannel);
 
                 String reply = readServerResponse(socketChannel);
+                logMessage("The server replied :" + System.lineSeparator() + reply, logsWriter);
 
-                if (!message.startsWith("play ")) {
-                    System.out.println("The server replied :" + System.lineSeparator() + reply);
-                } else {
+                if (message.startsWith("play")) {
                     try {
-                        System.out.println("Playing Song:");
                         constructSourceDataLine(reply);
                     } catch (LineUnavailableException e) {
-                        System.out.println("Streaming failed");
+                        logException(new Exception("Streaming failed", e), logsWriter);
                     }
                 }
+            }
+
+            try {
+                stopSong();
+                logMessage("Song stopped", logsWriter);
+            } catch (OperationNotSupportedException ignore) {
+                // No Song is Playing
+            } finally {
+                logMessage("Ending Program", logsWriter);
             }
 
         } catch (IOException e) {
             System.out.println("The Server is offline");
         }
+    }
 
-        stopSong();
+    private void logMessage(String message, PrintWriter logger) {
+        System.out.println(message);
+        System.out.println();
+
+        logger.println(message);
+        logger.println();
+    }
+
+    private void logException(Exception e, PrintWriter logger) {
+        e.printStackTrace(logger);
+        logger.println();
     }
 
     public void constructSourceDataLine(String reply) throws LineUnavailableException {
         String[] splitReply = reply.split("\\s+");
 
         if (!splitReply[RESPONSE_STATUS_INDEX].equalsIgnoreCase("ok")) {
-            System.out.println(reply);
             return;
         }
 
@@ -95,17 +123,17 @@ public class SpotifyClient implements Runnable {
 
         sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
         sourceDataLine.open();
+
+        System.out.println("Playing Song:");
         new Thread(new SongListener(streamingPort, sourceDataLine, this), "Song Listener").start();
     }
 
-    private void stopSong() {
+    private void stopSong() throws OperationNotSupportedException {
         if (sourceDataLine == null) {
-            System.out.println("Nothing to Stop");
-            return;
+            throw new OperationNotSupportedException("Nothing to stop");
         }
 
         sourceDataLine.stop();
-        System.out.println("Song stopped");
     }
 
     public void resetSourceDataLine() {
@@ -113,20 +141,22 @@ public class SpotifyClient implements Runnable {
     }
 
     private void writeToServer(String message, SocketChannel socketChannel) throws IOException {
-        buffer.clear(); // switch to writing mode
-        buffer.put(message.getBytes()); // buffer fill
-        buffer.flip(); // switch to reading mode
-        socketChannel.write(buffer); // buffer drain
+        buffer.clear();
+        buffer.put(message.getBytes());
+
+        buffer.flip();
+        socketChannel.write(buffer);
     }
 
     private String readServerResponse(SocketChannel socketChannel) throws IOException {
-        buffer.clear(); // switch to writing mode
-        socketChannel.read(buffer); // buffer fill
-        buffer.flip(); // switch to reading mode
+        buffer.clear();
+        socketChannel.read(buffer);
 
+        buffer.flip();
         byte[] byteArray = new byte[buffer.remaining()];
         buffer.get(byteArray);
-        return new String(byteArray, "UTF-8"); // buffer drain
+
+        return new String(byteArray, StandardCharsets.UTF_8);
     }
 
     public SourceDataLine getSourceDataLine() {
